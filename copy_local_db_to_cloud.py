017220976afe44517ee04ac368fc207facdfb040
adonoho <github@store.ddg.com>
2023-10-27 15:19:59 -0500

    Copy from the local EMS database to GBQ.

---
+++ copy_local_db_to_cloud.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from pandas import read_sql_table
+import sqlalchemy as sa
+from EMS.manager import get_gbq_credentials
+
+
+def copy_table_to_cloud(table_name: str):
+    engine = sa.create_engine('sqlite:///data/EMS.db3', echo=True)
+    ldb = engine.connect()  # ldb == Local Database. 'db' will be the remote persistent db.
+
+    df = read_sql_table(table_name, ldb, index_col='index')
+    # df.to_csv(f'{table_name}.csv')
+
+    df.to_gbq(f'EMS.{table_name}',
+              if_exists='append',
+              progress_bar=False,
+              credentials=get_gbq_credentials())
+
+    ldb.close()
+    engine.dispose()
+
+
+if __name__ == "__main__":
+    copy_table_to_cloud('mc-0010')
