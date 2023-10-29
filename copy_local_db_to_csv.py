c59e0de0591205b6d2c40cf1971e3ad1fbf85ee2
adonoho <github@store.ddg.com>
2023-10-29 10:21:33 -0500

    Added to_csv script.

--- copy_local_db_to_csv.py
+++ copy_local_db_to_csv.py
@@ -6,17 +6,12 @@ from EMS.manager import get_gbq_credentials
 
 
 def copy_table_to_gbq(table_name: str):
-    engine = sa.create_engine('sqlite:///data/EMS.db3', echo=True)
+    engine = sa.create_engine('sqlite:///data/EMS.db3', echo=False)
     ldb = engine.connect()  # ldb == Local Database. 'db' will be the remote persistent db.
 
     df = read_sql_table(table_name, ldb)
     df.drop(columns=['index'])
-    # df.to_csv(f'{table_name}.csv')
-
-    df.to_gbq(f'EMS.{table_name}',
-              if_exists='append',
-              progress_bar=False,
-              credentials=get_gbq_credentials())
+    df.to_csv(f'{table_name}.csv')
 
     ldb.close()
     engine.dispose()
